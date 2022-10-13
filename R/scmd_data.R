c90fb43d52d1a94113f291d629a8070b9f492273
kgray1 <67017663+kgray1@users.noreply.github.com>
2022-10-13 15:24:58 +0100

    updated scmd_data

--- R/scmd_data.R
+++ R/scmd_data.R
@@ -8,21 +8,41 @@
 #' @importFrom openxlsx read.xlsx
 #' @importFrom dplyr mutate
 #' @importFrom dplyr bind_rows
+#' @importFrom tidyr pivot_longer
 #'
 #' @examples
 #' scmd_data(file = "path")
 scmd_data <- function(
   file
 ) {
-  sheets <- openxlsx::getSheetNames(file)
+  sheetNames <- openxlsx::getSheetNames("Y:\\Official Stats\\PCHC\\data\\STP_BNF_2021.xlsx")
 
-  df <- data.frame()
+  icb_bnf_data <- data.frame()
 
-  for (i in sheets) {
-    tmp <- openxlsx::read.xlsx(file, sheet = i)
+  for(i in sheetNames) {
 
-    tmp |> dplyr::mutate(financial_year = i)
+    df <- readxl::read_excel(
+      "Y:\\Official Stats\\PCHC\\data\\STP_BNF_2021.xlsx",
+      sheet = i
+    ) %>%
+      mutate(
+        FINANCIAL_YEAR = gsub(" ", "/", i)
+      ) %>%
+      relocate(FINANCIAL_YEAR) %>%
+      rowwise() %>%
+      mutate(`Unknown ICB` = sum(across(contains("...")), na.rm = T)) %>%
+      rename(
+        BNF_SECTION = 2
+      ) %>%
+      tidyr::pivot_longer(cols = -c(FINANCIAL_YEAR, BNF_SECTION),
+                          names_to = "STP",
+                          values_to = "COST") %>%
+      dplyr::filter(!grepl("\\...", STP))
 
-    df <- dplyr::bind_rows(df, tmp)
-  }
+    icb_bnf_data <- icb_bnf_data %>%
+      bind_rows(
+        df
+      )
+    }
+  return(icb_bnf_data)
 }
