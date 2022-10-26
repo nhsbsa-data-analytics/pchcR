9883cb089cab3030a7a70622f637fa9b28a22f64
GPlatten <32270079+GPlatten@users.noreply.github.com>
2022-10-26 12:06:53 +0100

    Scmd data (#12)
    * remove create_fact documentation
    
    * create scmd_data function
    
    * updated scmd_data
    
    * Build scmd_icb_bnf_data function
    
    * Rebuild package
    
    * Minor updates to scmd_icb_bnf_data.R
    
    Co-authored-by: Matthew Wilson <matthew.wilson4@nhs.net>
    Co-authored-by: kgray1 <67017663+kgray1@users.noreply.github.com>

---
+++ R/scmd_icb_bnf_data.R
@@ -0,0 +1,80 @@
+#' Manipulate SCMD data
+#'
+#' @param file
+#'
+#' @return a data frame
+#' @export
+#'
+#' @import dplyr
+#'
+#' @importFrom openxlsx getSheetNames read.xlsx
+#' @importFrom tidyr pivot_longer
+#' @importFrom stringr str_extract str_extract_all str_length
+#'
+#' @examples
+#' scmd_icb_bnf_data(file = "C:/ICB_BNF_2022.xlsx")
+scmd_icb_bnf_data <- function(
+  file
+) {
+  sheetNames <- openxlsx::getSheetNames(file)
+
+  icb_bnf_data <- data.frame()
+
+  for(i in sheetNames) {
+
+  df <- readxl::read_excel(
+    file,
+    sheet = i
+  ) |>
+    dplyr::mutate(
+      FINANCIAL_YEAR = gsub(" ", "/", i)
+    ) |>
+    dplyr::relocate(FINANCIAL_YEAR) |>
+    #add .1 to incontinence appliances as needed for future manipulations to BNF chapter and section
+    mutate(
+      `Cost (£)` = case_when(
+        `Cost (£)` == "22 - Incontinence Appliances" ~ "22.1 - Incontinence Appliances",
+        TRUE ~ `Cost (£)`
+      )
+    )
+
+  #rename ICB columns to remove number in brackets
+  names(df)[3:44] <- substr(names(df)[3:44],1,nchar(names(df)[3:44])-4)
+
+  #build data
+  df2 <- df |>
+    dplyr::rowwise() |>
+    # create unknown ICB column which is sum of unallocated costs
+    dplyr::mutate(`Unknown ICB` = sum(across(contains("...")), na.rm = T))|>
+    dplyr::rename(
+      BNF = 2
+    ) |>
+    tidyr::pivot_longer(cols = -c(FINANCIAL_YEAR, BNF),
+                        names_to = "ICB",
+                        values_to = "COST") |>
+    dplyr::filter(!grepl("\\...", ICB)) |>
+    dplyr::mutate(
+      BNF_CHAPTER = stringr::str_extract(BNF, "[^\\.]+"),
+      BNF_SECTION = as.character(stringr::str_extract_all(BNF,"(?<=\\.).+(?= -)")),
+      BNF_CHAPTER = case_when(
+        stringr::str_length(BNF_CHAPTER) == 1 ~ paste0("0",BNF_CHAPTER),
+        TRUE ~ BNF_CHAPTER
+      ),
+      BNF_SECTION = case_when(
+        stringr::str_length(BNF_SECTION) == 1 ~ paste0("0",BNF_SECTION),
+        TRUE ~ BNF_SECTION
+      ),
+      BNF_SECTION = paste0(BNF_CHAPTER, BNF_SECTION)
+    ) |>
+    dplyr::mutate(
+      COST = case_when(
+        is.na(COST) ~ 0,
+        TRUE ~ COST
+      )
+    )
+  icb_bnf_data <- icb_bnf_data %>%
+    dplyr::bind_rows(df2)
+  }
+
+  return(icb_bnf_data)
+}
