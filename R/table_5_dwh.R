ed2e8bebe33b89f9632b8125ec4b73e094c057ca
GPlatten <32270079+GPlatten@users.noreply.github.com>
2022-10-26 14:21:51 +0100

    table_5_dwh function build

--- R/table_5_dwh.R
+++ R/table_5_dwh.R
@@ -1,9 +1,9 @@
-#' @title Easy helper for 'table_4_dwh'
+#' @title Easy helper for 'table_5_dwh'
 #'
-#' @name table_4_dwh
+#' @name table_5_dwh
 #'
 #' @description
-#' Extract DWH data to be used in table_4
+#' Extract DWH data to be used in table_5
 #'
 #' @param con The database connection object to be used
 #'
@@ -13,9 +13,9 @@
 #'
 #' @export
 #'
-#' @example table_4_dwh(con)
+#' @example table_5_dwh(con)
 
-table_4_dwh <- function(
+table_5_dwh <- function(
     con
     ) {
 
@@ -25,7 +25,9 @@ fact <- dplyr::tbl(src = con,
     FINANCIAL_YEAR,
     LVL_5_LTST_TYPE,
     BNF_CHAPTER,
-    CHAPTER_DESCR
+    CHAPTER_DESCR,
+    BNF_SECTION,
+    SECTION_DESCR
   ) |>
   dplyr::summarise(
     ITEM_PAY_DR_NIC = sum(ITEM_PAY_DR_NIC, na.rm = T),
@@ -37,16 +39,19 @@ fact <- dplyr::tbl(src = con,
   ) |>
   dplyr::arrange(
     FINANCIAL_YEAR,
-    BNF_CHAPTER
+    BNF_CHAPTER,
+    BNF_SECTION
   ) |>
   dplyr::collect() |>
   dplyr::rename(
     "Financial Year" = 1,
     "BNF Chapter" = 2,
     "BNF Chapter Description" = 3,
-    "Primary care prescribing dispensed in the community (GBP)" = 4,
-    "Dental prescribing dispensed in the community (GBP)" = 5,
-    "Hospital prescribing dispensed in the community (GBP)" = 6
+    "BNF Section" = 4,
+    "BNF Section Description" = 5,
+    "Primary care prescribing dispensed in the community (GBP)" = 6,
+    "Dental prescribing dispensed in the community (GBP)" = 7,
+    "Hospital prescribing dispensed in the community (GBP)" = 8
   )
 
 return(fact)
